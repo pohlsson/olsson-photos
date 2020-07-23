e016920b2bc7ea3546c232561f7bca21da8d375f
Patrik Olsson <patrik.olsson@axakon.se>
2020-07-23 18:41:42 +0200

    test

--- src/components/AlbumGrid/AlbumGrid.jsx
+++ src/components/AlbumGrid/AlbumGrid.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import styles from './AlbumGrid.module.scss';
 
 const AlbumGrid = ({photos, className}) => {
+    console.log(process.env.REACT_APP_ADMIN_PAWWSORD);
     return (
         <div className={className}>
             {photos.map(photo => (
